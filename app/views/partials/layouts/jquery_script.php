fb5eb112941190143ef1f5eb4a2530ea14f87ad4
Dennis Nygren <info@dennisnygren.se>
2014-01-18 15:28:36 -0800

    Deleted old stuff.

--- app/views/partials/layouts/jquery_script.php
+++
@@ -1,7 +0,0 @@
-<!--[if lt IE 9]>
-	<script src="<?= URL::asset('/js/jquery_old.js'); ?>"></script>
-<![endif]-->
-
-<!--[if gte IE 9]><!-->
-	<script src="<?= URL::asset('/js/jquery.js'); ?>"></script>
-<!--<![endif]-->
